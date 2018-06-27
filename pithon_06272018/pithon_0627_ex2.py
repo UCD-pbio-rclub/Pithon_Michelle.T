7a6a418d397406301467f71d7c7c2753503d43b0
melletang <mitang@ucdavis.edu>
2018-06-27 02:13:07 -0700

    upload ex2

---
+++ pithon_06272018/pithon_0627_ex2.py
@@ -0,0 +1,17 @@
+### 2. Write a function which takes a sentence as input and returns the reverse order of the words. ie "these are words" --> "words are these". Bonus: Capitalize the the first letter of the first word and end the returned string with the same punctuation as the input string. ie "these are words!" --> "Words are these!"
+
+
+def rev_sent():
+	sent = input('Sentence to reverse').lower()
+	if sent[-1].isalpha() == True: #if there is no punctuation
+		rev_sent = sent.split()[::-1]
+		rev_sent[0] = rev_sent[0].capitalize() #make first word cap
+		print(' '.join(rev_sent))
+	else:
+		punct = sent[-1]
+		sent = sent.replace(punct, '')
+		rev_sentlist = sent.split()[::-1]
+		rev_sentlist[0] = rev_sentlist[0].capitalize()
+		print(' '.join(rev_sentlist) + punct)
+	
+
